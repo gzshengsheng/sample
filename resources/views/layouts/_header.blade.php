f15a52bc7e3fdc802946d0584780b961ad9ed10d
gzshengsheng <gzshengsheng@139.com>
2018-11-01 07:24:10 +0000

    201811011524

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,4 +1,4 @@
-<header class="navbar navbar-fixed-top navbar-inverse">
+<header class="navbar navbar-inverse">
   <div class="container">
     <div class="col-md-offset-1 col-md-10">
       <a href="{{ route('home') }}" id="logo">Sample App</a>
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.default')
-@section('title','注册')
+@section('title', '注册')
+
 @section('content')
 <h1>注册</h1>
 @stop
